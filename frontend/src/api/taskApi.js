1c354fff0689613169fb726ab6e69648b93faa1f
Padalalavanya4319 <padalalavanya710@gmail.com>
2026-06-25 20:40:43 +0530

    third

--- frontend/src/api/taskApi.js
+++ frontend/src/api/taskApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/tasks",
+  baseURL: `${import.meta.env.VITE_API_BASE_URL || "/api"}/tasks`,
 });
 
 export const STORAGE_KEY = "taskflow-tasks";
